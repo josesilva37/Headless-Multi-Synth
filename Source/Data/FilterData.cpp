072918e670fa3cd4f859715f1c06c9fdbc0c3ef6
José Silva <63546835+josesilva37@users.noreply.github.com>
2024-03-04 18:23:56 +0000

    Implement FIlter Class

--- Source/Data/FilterData.cpp
+++ Source/Data/FilterData.cpp
@@ -1,1 +1,40 @@
+#include "FilterData.h"
 
+FilterData::FilterData()
+{
+    setType(juce::dsp::StateVariableTPTFilterType::lowpass);
+};
+
+void FilterData::prepareToPlay(juce::dsp::ProcessSpec spec)
+{
+    reset();
+    prepare(spec);
+};
+
+void FilterData::setParams(const int type, const float frequency, const float resonance)
+{
+    setCutoffFrequency(frequency);
+    setResonance(resonance);
+    selectFilterType(type);
+};
+
+void FilterData::selectFilterType(int type){
+ switch (type)
+    {
+    case 0:
+        setType(juce::dsp::StateVariableTPTFilterType::lowpass);
+        filterType = 0;
+    case 1:
+        setType(juce::dsp::StateVariableTPTFilterType::bandpass);
+        filterType = 1;
+
+    case 2:
+        setType(juce::dsp::StateVariableTPTFilterType::highpass);
+        filterType = 2;
+    }
+}
+
+void FilterData::processNextBlock(juce::AudioBuffer<float>& buffer){
+    juce::dsp::AudioBlock<float> block {buffer};
+    process(juce::dsp::ProcessContextReplacing<float>(block));
+}   
--- Source/Data/FilterData.h
+++ Source/Data/FilterData.h
@@ -2,7 +2,14 @@
 
 #include <JuceHeader.h>
 
-class FilterData
+class FilterData : public juce::dsp::StateVariableTPTFilter<float>
 {
-     
-}
+    public:
+        FilterData();
+        void setParams(const int type ,const float frequency, const float resonance );
+        void prepareToPlay(juce::dsp::ProcessSpec spec);
+        void processNextBlock(juce::AudioBuffer<float>& buffer);
+        void selectFilterType(int filterType);
+    private:
+     int filterType = 0;
+};
